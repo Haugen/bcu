9fab65d074a67e7c92ec2781c2c3c7932fe89459
Haugen <tobiashaugen@gmail.com>
2024-05-18 16:20:04 +0200

    git branch output to slice

--- main.go
+++ main.go
@@ -1,9 +1,27 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os/exec"
+	"strings"
 )
 
 func main() {
-	fmt.Println("Hey world etc!")
+	cmdResult, err := exec.Command("git", "branch").Output()
+
+	if err != nil {
+		fmt.Printf("error %s", err)
+	}
+
+	scanner := bufio.NewScanner(strings.NewReader(string(cmdResult)))
+	scanner.Split(bufio.ScanWords)
+	var branches []string
+	for scanner.Scan() {
+		text := scanner.Text()
+		if text != "*" && text != "main" && text != "master" {
+			branches = append(branches, text)
+		}
+	}
+	fmt.Println(branches)
 }
